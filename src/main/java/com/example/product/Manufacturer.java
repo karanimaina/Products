7c73ae1c99b6fd03275842137a06f2ef05d3ddf6
fkarani <maina.felix@eclectics.io>
2024-04-12 16:22:04 +0300

    enable tracking of prices frommanufacturers

--- src/main/java/com/example/product/Manufacturer.java
+++ src/main/java/com/example/product/Manufacturer.java
@@ -2,13 +2,11 @@ package com.example.product;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -16,16 +14,9 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
-public class Product extends BaseEntity{
+public class Manufacturer  extends  BaseEntity{
 private String name;
-private String description;
-private String quantity;
-private Date expiryDate;
-private Date purchaseDate;
-private int price;
-@OneToOne
-private Category category;
-@OneToMany (mappedBy = "product")
+private String location;
+@OneToMany (mappedBy = "manufacturer")
 private List<PriceHistory> priceHistories;
 }
-
